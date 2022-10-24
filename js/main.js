b84c6c0bded638f699c51ff2f82d45037dfa960a
huynhquocpho <huynhquocpho@gmail.com>
2022-10-24 15:44:03 +0700

    add the back-to-top button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 document.getElementById("switchTheme").onclick = function () {
   //Khi nút switchButton được ấn
-  //Tìm tất cả các element có id là " switchTheme " 
+  //Tìm tất cả các element có id là " switchTheme "
   //Thêm class "theme-dark" vào những element đó
   document.getElementById("changeTheme").classList.toggle("theme-dark");
   //dựa vào class theme-dark này để css cho các thẻ con có nền màu đen
 };
+// Back-to-top btn
+// Get the button:
+let backToTop = document.getElementById("backToTopBtn");
+// When the user scrolls down 40px from the top of the document, show the button
+window.onscroll = function () {
+  showBackToTopBtn();
+};
+function showBackToTopBtn() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function backToTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
